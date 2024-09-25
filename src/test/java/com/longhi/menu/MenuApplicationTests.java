a425c179f1f6ac709e07ac75b8b23bdce5257bec
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-09-24 21:03:08 -0300

    Initial Commit

---
+++ src/test/java/com/longhi/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longhi.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
